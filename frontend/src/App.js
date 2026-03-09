118a754bc1dc8e99e77ee9719c43357693255f54
sadbhavSK <sadbhavkadvade@gmail.com>
2026-03-09 19:49:07 +0530

    final working version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import login from "./pages/login"
-import Dashboard  from "./pages/Dashboard"
-import ProtectedRoute from "./components/ProtectedRoute"
-
-function App(){
-  return(
+function App() {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path = "/login" element = {<login/>} />
+      <Routes>
 
-      <Route path = "/dashboard"
-      element ={
-        <ProtectedRoute>
-          <Dashboard/>
-        </ProtectedRoute>
-      }
-      />
-    </Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
+
 export default App;
